eb42797405d11e4e9ee1f64949c25d311c75dc06
LeilaFarias <leilafarias1324@gmail.com>
2025-02-25 15:48:28 -0300

    Refactor: Consolidate multiple components into a single component

--- src/components/InitialBannerSection/InitialBannerSection.styles.tsx
+++ src/components/InitialBannerSection/InitialBannerSection.styles.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Section } from "@/app/globalStyles";
 import styled from "styled-components";
+import { Icon } from "../Icon/Icon";
 
 export const Wrapper = styled(Section)`
   height: 100vh;
@@ -14,10 +15,58 @@ export const MainWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 2.5rem;
-  width: 50vw;
+  width: 30rem;
 
   @media (max-width: 800px) {
-    width: 80vw;
-    height: 100vh;
+    width: 20rem;
   }
+`;
+
+export const ContentWrapper = styled.div<{ reduced: boolean }>`
+  display: flex;
+  flex-direction: ${({ reduced }) => (reduced ? "row" : "column")};
+  align-items: center;
+  justify-content: center;
+  gap: ${({ reduced }) => (reduced ? "1rem" : "2rem")};
+  flex-wrap: wrap;
+  width: ${({ reduced }) => (reduced ? "auto" : "60rem")};
+
+  @media (max-width: 1000px) {
+    justify-content: center;
+    gap: ${({ reduced }) => (reduced ? "1rem" : "1.5rem")};
+    width: auto;
+  }
+`;
+
+export const LogoImage = styled(Icon)<{ reduced: boolean }>`
+  @media (max-width: 800px) {
+    width: ${({ reduced }) => (reduced ? "4rem" : "15rem")};
+    height: auto;
+  }
+`;
+
+export const SectionTitle = styled.h2<{ reduced: boolean }>`
+  font-size: ${({ reduced }) => (reduced ? "1.5rem" : "2.5rem")};
+  font-weight: ${({ reduced }) => (reduced ? "700" : "900")};
+  text-align: center;
+  letter-spacing: 5%;
+  width: fit-content;
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+export const SubTitle = styled.h2`
+  color: ${({ theme }) => theme.colors.black};
+  align-self: center;
+  text-align: center;
+  width: fit-content;
+  font-size: 2rem;
+  font-weight: 500;
+  font-style: italic;
+`;
+
+export const TitleWrapper = styled.div<{ alignment: string }>`
+  display: flex;
+  flex-flow: column;
+  gap: 2rem;
+  align-items: ${({ alignment }) => alignment};
 `;
--- src/components/InitialBannerSection/InitialBannerSection.tsx
+++ src/components/InitialBannerSection/InitialBannerSection.tsx
@@ -1,16 +1,46 @@
-import InitialBanner from "./InitialBanner/InitialBanner";
-import { Wrapper, MainWrapper } from "./InitialBannerSection.styles";
+import { capitalize } from "@/utils/functions";
+import {
+  Wrapper,
+  MainWrapper,
+  ContentWrapper,
+  LogoImage,
+  SectionTitle,
+  SubTitle,
+} from "./InitialBannerSection.styles";
 import { ISectionHeader } from "@/utils/interfaces";
 
 const InitialBannerSection = ({
   sectionHead,
+  reduced = false,
 }: {
   sectionHead: ISectionHeader[];
+  reduced?: boolean;
 }) => {
+  const header = sectionHead?.find(
+    (head: ISectionHeader) => head.fields.id === "initialBanner",
+  );
+
+  const { title, subtitle } = header?.fields || {
+    title: "",
+    subtitle: undefined,
+  };
+
   return (
     <Wrapper full={"true"} id="initialBanner">
       <MainWrapper>
-        <InitialBanner sectionHead={sectionHead} />
+        <ContentWrapper reduced={reduced}>
+          <LogoImage
+            id="logo-datane"
+            width={reduced ? 62 : 350}
+            height={reduced ? 62 : 350}
+            reduced={true}
+          />
+
+          <SectionTitle reduced={reduced}>
+            {reduced ? capitalize(title) : title.toUpperCase()}
+          </SectionTitle>
+          {!reduced && subtitle && <SubTitle>{subtitle}</SubTitle>}
+        </ContentWrapper>
       </MainWrapper>
     </Wrapper>
   );
