8e845decb35e0c132922b9f125b5cdd8e8d956c3
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2025-03-04 14:00:10 -0300

    Fix: Fixes dependency issues when building

---
+++ declarations.d.ts
@@ -0,0 +1,5 @@
+// declarations.d.ts (or glide.d.ts)
+declare module "@glidejs/glide" {
+  const Glide: any;
+  export default Glide;
+}
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import Glide from "@glidejs/glide";
 import "@glidejs/glide/dist/css/glide.core.min.css";
 import "@glidejs/glide/dist/css/glide.theme.min.css";
+
+import Glide from "@glidejs/glide";
+
 import {
   Button,
   Card,
