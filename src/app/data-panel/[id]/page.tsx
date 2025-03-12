4a833f1a020416f10dd0c2674837b6b4a729350d
LeilaFarias <leilafarias1324@gmail.com>
2025-03-12 17:23:56 -0300

    fix: handle nonexistent ID in DataPanel route

--- src/app/data-panel/[id]/page.tsx
+++ src/app/data-panel/[id]/page.tsx
@@ -2,6 +2,7 @@ import PowerBIContainer from "@/components/PowerBIContainer/PowerBiContainer";
 import HubTemplate from "@/templates/HubTemplate";
 import { getContent } from "@/utils/functions";
 import { Container } from "./styles";
+import { notFound } from "next/navigation";
 
 export default async function DataPanel({
   params,
@@ -12,6 +13,10 @@ export default async function DataPanel({
   const selectedPanel = panels.find(
     (panel: any) => panel.fields.reportId === params.id,
   );
+
+  if (!selectedPanel) {
+    notFound();
+  }
 
   return (
     <HubTemplate>
