a8724590d22c66b627016c0cc60babc55b9170c3
cilasmarques <cilas@copin.ufcg.edu.br>
2025-03-31 17:12:24 -0300

    chore: throw back the macro theme title

--- src/components/PowerBIContainer/PowerBiContainer.tsx
+++ src/components/PowerBIContainer/PowerBiContainer.tsx
@@ -1,18 +1,22 @@
 "use client";
 import { ReportData } from "@/utils/interfaces";
+import { Wrapper, Title } from "./PowerBiContainer.styles";
 
 const PowerBIContainer = ({ panel }: { panel: { fields: ReportData } }) => {
-  const { title, source } = panel.fields;
+  const { macroTheme, title, source } = panel.fields;
 
   return (
-    <iframe
-      src={source}
-      title={title}
-      width="100%"
-      height="100%"
-      allowFullScreen={true}
-      style={{ border: "none" }}
-    />
+    <Wrapper>
+      <Title>{macroTheme}</Title>
+      <iframe
+        src={source}
+        title={title}
+        width="100%"
+        height="100%"
+        allowFullScreen={true}
+        style={{ border: "none" }}
+      />
+    </Wrapper>
   );
 };
 
