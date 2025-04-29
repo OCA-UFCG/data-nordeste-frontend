e81e2ea69b58453e6e4c248d017ab67ff86dd26f
cilasmarques <cilas@copin.ufcg.edu.br>
2025-04-29 19:29:59 -0300

    chore: remove radix components

--- src/components/ContentPost/ContentPost.tsx
+++ src/components/ContentPost/ContentPost.tsx
@@ -1,8 +1,7 @@
 import { IPublication } from "@/utils/interfaces";
 import { POST_TYPES } from "@/utils/constants";
-import { Box, Card, Text, Strong, Flex } from "@radix-ui/themes";
-import { ChevronRightIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const ContentPost = ({ content }: { content: { fields: IPublication } }) => {
   const { title, thumb, link, date, type, description } = content.fields;
@@ -11,39 +10,39 @@ const ContentPost = ({ content }: { content: { fields: IPublication } }) => {
 
   return (
     <Link href={link} className="block">
-      <Card className="rounded-t-md overflow-hidden m-0">
-        <Box className="relative group">
+      <div className="rounded-t-md overflow-hidden m-0 border border-gray-200 shadow-sm">
+        <div className="relative group">
           <img
             alt=""
             src={`https:${thumb.fields.file.url}`}
             className="h-[225px] w-full block object-cover transition-opacity duration-300 group-hover:opacity-75"
           />
-          <Box className="absolute inset-0 flex items-center justify-center bg-black opacity-0 group-hover:opacity-80 transition-opacity duration-300">
-            <Text className="text-white text-sm p-4 text-center drop-shadow-sm">
+          <div className="absolute inset-0 flex items-center justify-center bg-black opacity-0 group-hover:opacity-70 transition-opacity duration-300">
+            <p className="text-white text-sm p-4 text-center drop-shadow-sm">
               {description || "Acessar conteúdo"}
-            </Text>
-          </Box>
-          <Box className="absolute top-2 left-2 bg-white rounded-lg px-2 bg-gray-50">
-            <Strong>
+            </p>
+          </div>
+          <div className="absolute top-2 left-2 bg-white rounded-lg px-2 bg-gray-50">
+            <span className="font-bold">
               {
                 POST_TYPES[
                   type as "additional-content" | "data-panel" | "newsletter"
                 ]
               }
-            </Strong>
-          </Box>
-        </Box>
+            </span>
+          </div>
+        </div>
 
-        <Box className="flex items-center justify-between bg-gray-50 px-4 py-3 h-[95px]">
-          <Flex direction="column" gap="0.5">
-            <Text className="block break-words whitespace-normal leading-tight overflow-hidden font-bold">
+        <div className="flex items-center justify-between bg-gray-50 px-4 py-3 h-[95px]">
+          <div className="flex flex-col gap-2">
+            <p className="block break-words whitespace-normal leading-tight overflow-hidden font-bold">
               {title}
-            </Text>
-            <Text className="text-[#7E797B] text-xs">{formattedDate}</Text>
-          </Flex>
-          <ChevronRightIcon />
-        </Box>
-      </Card>
+            </p>
+            <p className="text-[#7E797B] text-xs">{formattedDate}</p>
+          </div>
+          <ArrowForwardIosIcon sx={{ fontSize: "1rem" }} />
+        </div>
+      </div>
     </Link>
   );
 };
