634afd144b45c27d1f6aa3600385677b5df22510
cilasmarques <cilas@copin.ufcg.edu.br>
2025-04-30 12:55:43 -0300

    chore: apply review suggestionss

--- src/components/ContentPost/ContentPost.tsx
+++ src/components/ContentPost/ContentPost.tsx
@@ -1,47 +1,43 @@
 import { IPublication } from "@/utils/interfaces";
 import { POST_TYPES } from "@/utils/constants";
 import Link from "next/link";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { Icon } from "@/components/Icon/Icon";
 
 const ContentPost = ({ content }: { content: { fields: IPublication } }) => {
-  const { title, thumb, link, date, type, description } = content.fields;
+  const { title, thumb, link, date, type } = content.fields;
   const dateObj = date ? new Date(date) : null;
   const formattedDate = dateObj ? dateObj.toLocaleDateString("pt-BR") : "";
 
   return (
-    <Link href={link} className="block">
-      <div className="rounded-t-md overflow-hidden m-0 border border-gray-200 shadow-sm">
-        <div className="relative group">
-          <img
-            alt=""
-            src={`https:${thumb.fields.file.url}`}
-            className="h-[225px] w-full block object-cover transition-opacity duration-300 group-hover:opacity-75"
-          />
-          <div className="absolute inset-0 flex items-center justify-center bg-black opacity-0 group-hover:opacity-70 transition-opacity duration-300">
-            <p className="text-white text-sm p-4 text-center drop-shadow-sm">
-              {description || "Acessar conteúdo"}
-            </p>
-          </div>
-          <div className="absolute top-2 left-2 bg-white rounded-lg px-2 bg-gray-50">
-            <span className="font-bold">
-              {
-                POST_TYPES[
-                  type as "additional-content" | "data-panel" | "newsletter"
-                ]
-              }
-            </span>
-          </div>
+    <Link
+      href={link}
+      className="flex flex-col overflow-hidden rounded-md w-full bg-grey-100 hover:bg-grey-200 border border-grey-200 hover:border-grey-300 cursor-pointer transition duration-300 shadow-sm"
+    >
+      <div className="relative group">
+        <img
+          alt=""
+          src={`https:${thumb.fields.file.url}`}
+          className="h-[180px] w-full block object-cover transition-transform group-hover:scale-102 duration-300"
+        />
+        <div className="absolute top-4 left-4 rounded-lg px-2 py-0.5 bg-gray-100">
+          <p className="font-semibold text-xs">
+            {
+              POST_TYPES[
+                type as "additional-content" | "data-panel" | "newsletter"
+              ]
+            }
+          </p>
         </div>
+      </div>
 
-        <div className="flex items-center justify-between bg-gray-50 px-4 py-3 h-[95px]">
-          <div className="flex flex-col gap-2">
-            <p className="block break-words whitespace-normal leading-tight overflow-hidden font-bold">
-              {title}
-            </p>
-            <p className="text-[#7E797B] text-xs">{formattedDate}</p>
-          </div>
-          <ArrowForwardIosIcon sx={{ fontSize: "1rem" }} />
+      <div className="p-5 flex justify-between items-center md:max-h-[76px]">
+        <div className="flex flex-col gap-0.5 md:max-w-[95%]">
+          <p className="line-clamp-2 leading-tight font-semibold text-base text-wrap">
+            {title}
+          </p>
+          <p className="text-[#7E797B] text-xs">{formattedDate}</p>
         </div>
+        <Icon className="hidden md:flex rotate-270" id="expand" size={9} />
       </div>
     </Link>
   );
