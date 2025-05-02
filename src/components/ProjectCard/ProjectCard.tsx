d0f62b8224175c8002abedb2b4fd9f9b33bba242
cilasmarques <cilas@copin.ufcg.edu.br>
2025-05-02 18:07:01 -0300

    chore: improve responsiveness

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -5,7 +5,7 @@ const ProjectCard = ({ project }: { project: { fields: Project } }) => {
   const { name, description, link, thumb } = project.fields;
 
   return (
-    <div className="flex flex-col md:flex-row gap-4 justify-between w-full max-w-[630px]">
+    <div className="flex flex-col md:flex-row gap-4 justify-between items-center w-full max-w-full md:max-w-[80%] lg:max-w-[48%] xl:max-w-[45%] mx-auto">
       <a href={link} className="w-full">
         <div className="relative w-full overflow-hidden rounded">
           <img
@@ -16,7 +16,7 @@ const ProjectCard = ({ project }: { project: { fields: Project } }) => {
         </div>
       </a>
 
-      <div className="flex flex-col justify-around w-full gap-2 items-center px-4 min-h-[200px]">
+      <div className="flex flex-col justify-around w-full gap-2 items-center px-4 min-h-[200px] md:min-h-0">
         <div className="flex flex-col gap-2 w-full">
           <h3 className="text-xl font-semibold text-gray-800">{name}</h3>
           <p className="text-sm text-grey-600 leading-5">{description}</p>
