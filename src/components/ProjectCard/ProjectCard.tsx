7bee66f25b104f57bd020e2711d0a5dd59324fa4
cilasmarques <cilas@copin.ufcg.edu.br>
2025-05-02 17:13:44 -0300

    refact: remove styled components usage

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,41 +1,31 @@
 import { Project } from "@/utils/interfaces";
-import {
-  Thumb,
-  Wrapper,
-  InfoWrapper,
-  TextWrapper,
-  Title,
-  Description,
-  Link,
-  ThumbContainer,
-} from "./ProjectCard.styles";
 import { LinkButton } from "../LinkButton/LinkButton";
 
 const ProjectCard = ({ project }: { project: { fields: Project } }) => {
   const { name, description, link, thumb } = project.fields;
 
   return (
-    <Wrapper>
-      <Link href={link}>
-        <ThumbContainer>
-          <Thumb
+    <div className="flex flex-col md:flex-row gap-4 justify-between w-full max-w-[630px]">
+      <a href={link} className="w-full">
+        <div className="relative w-full overflow-hidden rounded">
+          <img
+            className="w-full h-full object-cover transition-transform duration-300 hover:scale-[1.01]"
             src={`https:${thumb.fields.file.url}`}
             alt=""
-            width={600}
-            height={300}
           />
-        </ThumbContainer>
-      </Link>
-      <InfoWrapper>
-        <TextWrapper>
-          <Title>{name}</Title>
-          <Description>{description}</Description>
-        </TextWrapper>
+        </div>
+      </a>
+
+      <div className="flex flex-col justify-around w-full gap-2 items-center px-4 min-h-[200px]">
+        <div className="flex flex-col gap-2 w-full">
+          <h3 className="text-xl font-semibold text-gray-800">{name}</h3>
+          <p className="text-sm text-grey-600 leading-5">{description}</p>
+        </div>
         <LinkButton href={link} className="md:w-full">
           Acessar
         </LinkButton>
-      </InfoWrapper>
-    </Wrapper>
+      </div>
+    </div>
   );
 };
 
--- src/components/ProjectCard/Section/ProjectSection.tsx
+++ src/components/ProjectCard/Section/ProjectSection.tsx
@@ -1,12 +1,5 @@
 import { Project, SectionHeader } from "@/utils/interfaces";
 import ProjectCard from "../ProjectCard";
-import {
-  Wrapper,
-  Projects,
-  Header,
-  Title,
-  Subtitle,
-} from "./ProjectSection.styles";
 
 export const ProjectSection = ({
   header,
@@ -15,23 +8,23 @@ export const ProjectSection = ({
   header?: { fields: SectionHeader };
   projects?: { fields: Project }[];
 }) => {
-  const { title, id, subtitle } = header?.fields || {
+  const { title, id } = header?.fields || {
     title: "",
     id: "",
     subtitle: "",
   };
 
   return (
-    <Wrapper id={id}>
-      <Header>
-        <Title>{title}</Title>
-        <Subtitle>{subtitle}</Subtitle>
-      </Header>
-      <Projects>
+    <section
+      id={id}
+      className="w-full max-w-[1440px] px-4 py-6 content-center flex flex-col gap-6 box-border"
+    >
+      <h2 className="text-3xl font-semibold">{title}</h2>
+      <div className="flex flex-wrap justify-center gap-10 w-full max-w-full">
         {projects?.map((partner: { fields: Project }) => (
           <ProjectCard key={partner.fields.name} project={partner} />
         ))}
-      </Projects>
-    </Wrapper>
+      </div>
+    </section>
   );
 };
