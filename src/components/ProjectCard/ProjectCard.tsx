6b39251e66b57f3127ccca3ff98fbbe06841cc37
cilasmarques <cilas@copin.ufcg.edu.br>
2025-05-05 12:53:00 -0300

    fix: image proportion

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -7,16 +7,14 @@ const ProjectCard = ({ project }: { project: { fields: Project } }) => {
 
   return (
     <div className="flex flex-col md:flex-row gap-4 justify-between items-center w-full max-w-full md:max-w-[80%] lg:max-w-[48%] xl:max-w-[45%] mx-auto bg-white border border-grey-200 rounded-md hover:scale-[1.01] transition-transform duration-300">
-      <a href={link} className="w-full">
-        <div className="relative w-full aspect-[4/3] md:aspect-[1/1]">
-          <Image
-            className="w-full h-full object-cover rounded-t-md md:rounded-l-md md:rounded-tr-none"
-            src={`https:${thumb.fields.file.url}`}
-            alt=""
-            width={600}
-            height={800}
-          />
-        </div>
+      <a href={link} className="w-full h-full">
+        <Image
+          className="w-full h-full object-cover rounded-t-md md:rounded-l-md md:rounded-tr-none"
+          src={`https:${thumb.fields.file.url}`}
+          alt=""
+          width={800}
+          height={800}
+        />
       </a>
 
       <div className="flex flex-col justify-between w-full h-full gap-2 items-center p-4 min-h-[200px] md:min-h-0">
