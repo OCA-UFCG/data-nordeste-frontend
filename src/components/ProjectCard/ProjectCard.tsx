086e48e612701efec077b6a558d9fc83399e296c
cilasmarques <cilas@copin.ufcg.edu.br>
2025-05-05 11:54:36 -0300

    chore: adjust image aspect ratios

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -8,9 +8,9 @@ const ProjectCard = ({ project }: { project: { fields: Project } }) => {
   return (
     <div className="flex flex-col md:flex-row gap-4 justify-between items-center w-full max-w-full md:max-w-[80%] lg:max-w-[48%] xl:max-w-[45%] mx-auto bg-white border border-grey-200 rounded-md hover:scale-[1.01] transition-transform duration-300">
       <a href={link} className="w-full">
-        <div className="relative w-full h-full">
+        <div className="relative w-full aspect-[4/3] md:aspect-[1/1]">
           <Image
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover rounded-t-md md:rounded-l-md md:rounded-tr-none"
             src={`https:${thumb.fields.file.url}`}
             alt=""
             width={600}
@@ -19,7 +19,7 @@ const ProjectCard = ({ project }: { project: { fields: Project } }) => {
         </div>
       </a>
 
-      <div className="flex flex-col justify-between w-full h-full gap-2 items-center px-4 py-2 min-h-[200px] md:min-h-0">
+      <div className="flex flex-col justify-between w-full h-full gap-2 items-center p-4 min-h-[200px] md:min-h-0">
         <div className="flex flex-col gap-2 w-full">
           <h3 className="text-xl font-semibold text-gray-800">{name}</h3>
           <p className="text-sm text-grey-600 leading-5">{description}</p>
