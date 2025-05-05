b07e7dabba12dd8ee4141650f51dcf9a19aa6065
cilasmarques <cilas@copin.ufcg.edu.br>
2025-05-05 11:18:45 -0300

    chore: apply review suggestions

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,22 +1,25 @@
 import { Project } from "@/utils/interfaces";
 import { LinkButton } from "../LinkButton/LinkButton";
+import Image from "next/image";
 
 const ProjectCard = ({ project }: { project: { fields: Project } }) => {
   const { name, description, link, thumb } = project.fields;
 
   return (
-    <div className="flex flex-col md:flex-row gap-4 justify-between items-center w-full max-w-full md:max-w-[80%] lg:max-w-[48%] xl:max-w-[45%] mx-auto">
+    <div className="flex flex-col md:flex-row gap-4 justify-between items-center w-full max-w-full md:max-w-[80%] lg:max-w-[48%] xl:max-w-[45%] mx-auto bg-white border border-grey-200 rounded-md hover:scale-[1.01] transition-transform duration-300">
       <a href={link} className="w-full">
-        <div className="relative w-full overflow-hidden rounded">
-          <img
-            className="w-full h-full object-cover transition-transform duration-300 hover:scale-[1.01]"
+        <div className="relative w-full h-full">
+          <Image
+            className="w-full h-full object-cover"
             src={`https:${thumb.fields.file.url}`}
             alt=""
+            width={600}
+            height={800}
           />
         </div>
       </a>
 
-      <div className="flex flex-col justify-around w-full gap-2 items-center px-4 min-h-[200px] md:min-h-0">
+      <div className="flex flex-col justify-between w-full h-full gap-2 items-center px-4 py-2 min-h-[200px] md:min-h-0">
         <div className="flex flex-col gap-2 w-full">
           <h3 className="text-xl font-semibold text-gray-800">{name}</h3>
           <p className="text-sm text-grey-600 leading-5">{description}</p>
--- src/components/ProjectCard/Section/ProjectSection.tsx
+++ src/components/ProjectCard/Section/ProjectSection.tsx
@@ -17,7 +17,7 @@ export const ProjectSection = ({
   return (
     <section
       id={id}
-      className="w-full max-w-[1440px] px-4 py-6 content-center flex flex-col gap-6 box-border"
+      className="w-full max-w-[1440px] px-4 py-6 content-center flex flex-col gap-6 box-border bg-grey-100"
     >
       <h2 className="text-3xl font-semibold">{title}</h2>
       <div className="flex flex-wrap justify-center gap-10 w-full max-w-full">
