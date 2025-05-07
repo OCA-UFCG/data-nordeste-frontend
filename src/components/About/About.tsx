8535345b8279ad377ebbb4d3d2a04f152b6e3020
LeilaFarias <leilafarias1324@gmail.com>
2025-05-07 11:29:58 -0300

    chore: remove usuless css and replace img for Image next's component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,5 @@
 import { LinkButton } from "../LinkButton/LinkButton";
+import Image from "next/image";
 
 export const AboutSection = ({ header }: { header: { fields: any } }) => {
   const { id, title, subtitle, thumb } = header.fields;
@@ -6,14 +7,16 @@ export const AboutSection = ({ header }: { header: { fields: any } }) => {
   return (
     <section id={id} className="w-full bg-grey-100 py-8 lg:py-12">
       <div className="w-full max-w-[1440px] mx-auto px-4 flex flex-col gap-6">
-        <h2 className="text-[30px] leading-[36px] tracking-[-0.0075em] font-bold text-left lg:text-left text-center">
+        <h2 className="text-[30px] leading-[36px] tracking-[-0.0075em] font-bold text-left">
           {title}
         </h2>
         <div className="flex flex-col lg:flex-row gap-6">
-          <img
+          <Image
             alt=""
             src={`https:${thumb.fields.file.url}`}
-            className="w-full lg:w-[462px] lg:h-[374px] rounded-lg"
+            width={500}
+            height={374}
+            className="w-fit max-h-[374px] rounded-lg"
           />
           <div className="flex flex-col items-start justify-between lg:max-h-[375px] lg:max-w-full gap-6">
             <p className="text-left whitespace-pre-line h-fit lg:overflow-auto">
