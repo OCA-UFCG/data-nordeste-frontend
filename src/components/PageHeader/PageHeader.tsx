7ccbdbb72a1c54fd8819c2ef77499a638dc5f357
LeilaFarias <leilafarias1324@gmail.com>
2025-05-12 13:00:30 -0300

    chore: adjust text size

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -6,9 +6,9 @@ const PageHeader = ({ content }: { content: { fields: IPageHeader } }) => {
   return (
     <section className="flex items-center justify-center bg-grey-100 w-full min-h-[200px] px-6 pt-9 lg:px-20 lg:pt-12 pb-9">
       <div className="flex flex-col items-left gap-6 max-w-[1440px]">
-        <h1 className="font-extrabold text-[30px] lg:text-4xl">{title}</h1>
+        <h1 className="font-extrabold text-[30px] lg:text-[48px]">{title}</h1>
 
-        <h2 className="font-medium">{subtitle}</h2>
+        <h2 className="text-[16px]">{subtitle}</h2>
       </div>
     </section>
   );
