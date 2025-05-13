0ff97e883e8e44ce6d047e49d2d89e331232f42e
cilasmarques <cilas@copin.ufcg.edu.br>
2025-05-13 13:11:56 -0300

    chore: update font

--- src/components/ProjectCard/FullProjectCard.tsx
+++ src/components/ProjectCard/FullProjectCard.tsx
@@ -16,7 +16,7 @@ const FullProjectCard = ({
   return (
     <section className="flex items-center justify-center w-full min-h-[200px] px-6 pt-9 lg:px-20 lg:pt-12 pb-9">
       <div className="flex flex-col items-left max-w-[1440px]">
-        <h2 className="text-semibold text-3xl py-4">{name}</h2>
+        <h2 className="font-semibold text-3xl py-4">{name}</h2>
         <div className="flex flex-col lg:flex-row w-full overflow-hidden justify-between">
           <div
             className={`text-base text-gray-800 h-full space-y-4 lg:w-[65%] ${isEvenIndex ? "lg:order-2 lg:pl-12" : "lg:order-1 lg:pr-12"}`}
