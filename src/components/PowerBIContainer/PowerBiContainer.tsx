3011bb098b3ec85679ac83d16b51f52dbd3fe6db
cilasmarques <cilas@copin.ufcg.edu.br>
2025-05-14 17:45:09 -0300

    chore: update data-panel header

--- src/components/PowerBIContainer/PowerBiContainer.tsx
+++ src/components/PowerBIContainer/PowerBiContainer.tsx
@@ -1,13 +1,5 @@
 "use client";
 import { ReportData } from "@/utils/interfaces";
-import {
-  Wrapper,
-  Title,
-  Header,
-  Home,
-  RightIcon,
-  Text,
-} from "./PowerBiContainer.styles";
 
 const PowerBIContainer = ({
   panel,
@@ -16,26 +8,26 @@ const PowerBIContainer = ({
   panel: { fields: ReportData };
   pageName?: string;
 }) => {
-  const { macroTheme, title, source } = panel.fields;
+  const { macroTheme, title, source, date } = panel.fields;
+  const dateObj = date ? new Date(date) : null;
+  const formattedDate = dateObj ? dateObj.toLocaleDateString("pt-BR") : "";
 
   return (
-    <Wrapper>
-      <Header>
-        <Home href="/">
-          <RightIcon id="expand" size={10} />
-          <Text>Página inicial</Text>
-        </Home>
-        <Title>{macroTheme}</Title>
-      </Header>
+    <div className="flex flex-col w-max h-full z-0 bg-white gap-2 overflow-x-scroll p-4">
+      <div className="flex items-center justify-between w-full py-4">
+        <h2 className="text-left font-semibold text-3xl">{macroTheme}</h2>
+        <span className="font-medium text-base mt-auto">
+          Publicado em: {formattedDate}
+        </span>
+      </div>
       <iframe
         src={pageName ? `${source}&pageName=${pageName}` : source}
-        title={title}
-        width="1220px"
-        height="100%"
         allowFullScreen={true}
-        style={{ border: "none" }}
+        title={title}
+        width="1184px"
+        height="785px"
       />
-    </Wrapper>
+    </div>
   );
 };
 
