138e43cae00f7b21a1954a288167066fb5f51661
cilasmarques <cilas@copin.ufcg.edu.br>
2025-05-14 14:22:06 -0300

    feat: add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,63 @@
+import HubTemplate from "@/templates/HubTemplate";
+import { getContent } from "@/utils/functions";
+import PageHeader from "@/components/PageHeader/PageHeader";
+import AboutCard from "@/components/AboutCard/AboutCard";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+
+export const revalidate = 60;
+
+export default async function Connections({}: {}) {
+  const { pageHeaders, about } = await getContent(["pageHeaders", "about"]);
+
+  return (
+    <HubTemplate>
+      <PageHeader
+        content={pageHeaders.find(
+          (section: { fields: { id: string } }) =>
+            section.fields.id === "about",
+        )}
+      />
+      <AboutCard about={about[0]} />
+      <div className="w-full px-6 pt-9 lg:px-20 lg:pt-12 pb-9">
+        <h2 className="text-green-900 text-2xl font-semibold">Galeria</h2>
+        <div className="flex justify-center">
+          <Carousel
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+            className="flex flex-col gap-4 content-carousel"
+          >
+            <CarouselContent className="-ml-0">
+              {(about[0].fields.album as any[]).map(
+                (photo: any, index: number) => (
+                  <CarouselItem
+                    key={index}
+                    className="basis-1/1 md:basis-1/2 lg:basis-1/3 xl:basis-1/4 p-0 md:p-2"
+                  >
+                    <Image
+                      width={250}
+                      height={250}
+                      alt=""
+                      src={`https:${photo?.fields?.file.url || ""}`}
+                      className="w-full max-h-[250px] rounded-lg aspect-1/1 block object-cover object-top transition-transform group-hover:scale-102 duration-300"
+                    />
+                  </CarouselItem>
+                ),
+              )}
+            </CarouselContent>
+            <CarouselPrevious className="hidden md:flex" />
+            <CarouselNext className="hidden md:flex" />
+          </Carousel>
+        </div>
+      </div>
+    </HubTemplate>
+  );
+}
---
+++ src/components/AboutCard/AboutCard.tsx
@@ -0,0 +1,30 @@
+import { About } from "@/utils/interfaces";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+const AboutCard = ({ about }: { about: { fields: About } }) => {
+  const { details, thumb } = about.fields;
+
+  return (
+    <section className="flex items-center justify-center w-full min-h-[200px] px-6 pt-9 lg:px-20 lg:pt-12">
+      <div className="flex flex-col items-left max-w-[1440px]">
+        <div className="flex flex-col lg:flex-row w-full overflow-hidden justify-between">
+          <div className="text-base text-gray-800 h-full max-h-[400px] overflow-y-auto space-y-4 lg:w-[65%] lg:order-2 lg:pl-12">
+            {documentToReactComponents(details)}
+          </div>
+          <div className="flex flex-col lg:w-[35%] mt-6 lg:mt-0 lg:order-1">
+            <Image
+              className="w-full h-full object-cover rounded-md aspect-[16/9] mb-4"
+              src={`https:${thumb.fields.file.url}`}
+              alt={"Sobre nós"}
+              width={800}
+              height={800}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutCard;
