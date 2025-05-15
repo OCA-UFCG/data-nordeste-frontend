c40984dd7c40652eb8eb8b432e0d9b2cfe68f624
cilasmarques <cilas@copin.ufcg.edu.br>
2025-05-15 19:00:42 -0300

    fix: update iframe aspect ratio

--- src/components/PowerBIContainer/PowerBiContainer.tsx
+++ src/components/PowerBIContainer/PowerBiContainer.tsx
@@ -13,21 +13,19 @@ const PowerBIContainer = ({
   const formattedDate = dateObj ? dateObj.toLocaleDateString("pt-BR") : "";
 
   return (
-    <div className="flex flex-col items-center w-full h-full z-0 bg-white p-4">
-      <div className="w-full max-w-[1024px] flex flex-col gap-4">
-        <div className="flex flex-col sm:flex-row items-center justify-between w-full py-4">
-          <h2 className="text-left font-semibold text-3xl">{macroTheme}</h2>
-          <span className="font-medium text-base mt-2 sm:mt-0">
-            Publicado em: {formattedDate}
-          </span>
-        </div>
-        <iframe
-          src={pageName ? `${source}&pageName=${pageName}` : source}
-          allowFullScreen
-          title={title}
-          className="w-full aspect-[32/25] h-auto"
-        />
+    <div className="flex flex-col w-full max-w-[1240px] h-full z-0 bg-white gap-2 overflow-x-scroll p-4">
+      <div className="flex flex-col sm:flex-row justify-between w-full py-4 sticky left-0">
+        <h2 className="text-left font-semibold text-3xl">{macroTheme}</h2>
+        <span className="font-medium text-base mt-2 sm:mt-0">
+          Publicado em: {formattedDate}
+        </span>
       </div>
+      <iframe
+        src={pageName ? `${source}&pageName=${pageName}` : source}
+        allowFullScreen
+        title={title}
+        className="w-full aspect-[32/25] h-auto"
+      />
     </div>
   );
 };
