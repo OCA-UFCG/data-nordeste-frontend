584235d043303295a15704ff0f6f80d4fa9566e8
cilasmarques <cilasmrq@gmail.com>
2025-05-19 22:13:05 -0300

    feat: create galley carousel component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,16 +2,9 @@ import HubTemplate from "@/templates/HubTemplate";
 import { getContent } from "@/utils/functions";
 import PageHeader from "@/components/PageHeader/PageHeader";
 import AboutBigCard from "@/components/AboutBigCard/AboutBigCard";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import Image from "next/image";
 import PageTabs from "@/components/PageTabs/PageTabs";
 import { Suspense } from "react";
+import GalleryCarousel from "@/components/GalleryCarousel/GalleryCarousel";
 
 export const revalidate = 60;
 
@@ -33,39 +26,7 @@ export default async function AboutPage({}: {}) {
         />
         <PageTabs content={pageTabs} />
         <AboutBigCard content={pageTabs} about={about[0]} />
-        <div className=" max-w-[1440px] py-6 mx-6 lg:mx-20">
-          <h2 className="text-green-900 text-2xl font-semibold">Galeria</h2>
-          <div className="flex justify-center">
-            <Carousel
-              opts={{
-                align: "start",
-                loop: true,
-              }}
-              className="flex flex-col gap-4 content-carousel"
-            >
-              <CarouselContent className="-ml-0">
-                {(about[0].fields.album as any[]).map(
-                  (photo: any, index: number) => (
-                    <CarouselItem
-                      key={index}
-                      className="basis-1/1 md:basis-1/2 lg:basis-1/3 xl:basis-1/4 p-0 md:p-2"
-                    >
-                      <Image
-                        width={250}
-                        height={250}
-                        alt=""
-                        src={`https:${photo?.fields?.file.url || ""}`}
-                        className="w-full max-h-[250px] rounded-lg aspect-1/1 block object-cover object-top transition-transform group-hover:scale-102 duration-300"
-                      />
-                    </CarouselItem>
-                  ),
-                )}
-              </CarouselContent>
-              <CarouselPrevious className="hidden md:flex" />
-              <CarouselNext className="hidden md:flex" />
-            </Carousel>
-          </div>
-        </div>
+        <GalleryCarousel album={about[0].fields.album} />
       </Suspense>
     </HubTemplate>
   );
