31724b411c6a5e4faf0e2248aa806bb35ce15200
cilasmarques <cilasmrq@gmail.com>
2025-05-21 22:44:29 -0300

    chore: update history box proportions

--- src/components/AboutBigCard/AboutBigCard.tsx
+++ src/components/AboutBigCard/AboutBigCard.tsx
@@ -11,13 +11,13 @@ const AboutBigCard = ({ content }: { content: { fields: About } }) => {
         <h2 className="font-semibold text-3xl">{id}</h2>
         <div className="flex flex-col lg:flex-row w-full overflow-hidden justify-between items-top my-5">
           <Image
-            className="w-full h-full max-h-[400px] object-cover rounded-md aspect-[3/4] order-1 mb-5"
+            className="w-full h-full max-h-[400px] object-cover rounded-md aspect-[4/3] order-1 mb-5"
             src={`https:${thumb.fields.file.url}`}
             alt={"Sobre nós"}
-            width={400}
-            height={400}
+            width={600}
+            height={600}
           />
-          <div className="h-full max-h-[400px] overflow-y-auto text-base text-gray-800 order-2 lg:pl-10">
+          <div className="h-full max-h-[400px] overflow-y-auto text-base text-gray-800 order-2 lg:pl-6">
             {documentToReactComponents(details)}
           </div>
         </div>
