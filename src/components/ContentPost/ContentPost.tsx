74f8d3669e7e544809e1b5b7ea1e29ff5e0aa633
cilasmarques <cilasmrq@gmail.com>
2025-05-26 10:38:01 -0300

    refact: update ContentPost label

--- src/components/ContentPost/ContentPost.tsx
+++ src/components/ContentPost/ContentPost.tsx
@@ -20,32 +20,26 @@ const ContentPost = ({
       href={link}
       className="group flex flex-col overflow-hidden rounded-md w-full bg-grey-100 hover:bg-grey-200 border border-grey-200 hover:border-grey-300 cursor-pointer transition duration-300 shadow-md h-full"
     >
-      <div className="relative group">
-        <Image
-          width={300}
-          height={300}
-          alt=""
-          src={`https:${thumb?.fields?.file?.url || ""}`}
-          className="w-full aspect-7/4 block object-cover object-top transition-transform group-hover:scale-102 duration-300"
-        />
-        {labeled && (
-          <div className="absolute top-4 left-4 rounded-lg px-2 py-0.5 bg-gray-100">
-            <p className="font-semibold text-xs">
-              {
-                POST_TYPES[
-                  type as "additional-content" | "data-panel" | "newsletter"
-                ]
-              }
-            </p>
-          </div>
-        )}
+      <Image
+        width={300}
+        height={300}
+        alt=""
+        src={`https:${thumb?.fields?.file?.url || ""}`}
+        className="w-full aspect-7/4 block object-cover object-top transition-transform group-hover:scale-102 duration-300"
+      />
+      <div className="flex flex-row justify-between bg-gray-200 px-2 py-1">
+        <p className="font-semibold text-xs">
+          {
+            POST_TYPES[
+              type as "additional-content" | "data-panel" | "newsletter"
+            ]
+          }
+        </p>
+        <p className="text-grey-600 text-xs">{formattedDate}</p>
       </div>
 
       <div className="py-4 px-5 flex gap-3 justify-between items-center h-full box-border">
-        <div className="flex flex-col gap-0.5">
-          <p className="line-clamp-2 text-sm font-medium text-wrap">{title}</p>
-          <p className="text-grey-600 text-xs">{formattedDate}</p>
-        </div>
+        <p className="line-clamp-2 text-sm font-medium text-wrap">{title}</p>
         <Icon
           className="md:flex rotate-270 size-2 min-w-2"
           id="expand"
