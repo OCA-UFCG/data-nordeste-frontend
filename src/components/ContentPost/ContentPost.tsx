ee91e8f9331774bb20b2350c7478a3bc9bef0da1
cilasmarques <cilasmrq@gmail.com>
2025-05-26 12:37:10 -0300

    chore: apply review suggestions

--- src/components/ContentPost/ContentPost.tsx
+++ src/components/ContentPost/ContentPost.tsx
@@ -14,31 +14,35 @@ const ContentPost = ({ content }: { content: { fields: IPublication } }) => {
       href={link}
       className="group flex flex-col overflow-hidden rounded-md w-full bg-grey-100 hover:bg-grey-200 border border-grey-200 hover:border-grey-300 cursor-pointer transition duration-300 shadow-md h-full"
     >
-      <Image
-        width={300}
-        height={300}
-        alt=""
-        src={`https:${thumb?.fields?.file?.url || ""}`}
-        className="w-full aspect-7/4 block object-cover object-top transition-transform group-hover:scale-102 duration-300"
-      />
-      <div className="flex flex-row justify-between bg-gray-200 px-5 py-1">
-        <p className="font-semibold text-xs">
-          {
-            POST_TYPES[
-              type as "additional-content" | "data-panel" | "newsletter"
-            ]
-          }
-        </p>
-        <p className="text-grey-600 text-xs">{formattedDate}</p>
-      </div>
-
-      <div className="py-4 px-5 flex gap-3 justify-between items-center h-full box-border">
-        <p className="line-clamp-2 text-sm font-medium text-wrap">{title}</p>
-        <Icon
-          className="md:flex rotate-270 size-2 min-w-2"
-          id="expand"
-          size={9}
+      <div className="w-full overflow-hidden">
+        <Image
+          width={300}
+          height={300}
+          alt=""
+          src={`https:${thumb?.fields?.file?.url || ""}`}
+          className="w-full aspect-7/4 block object-cover object-top transition-transform group-hover:scale-102 duration-300"
         />
+      </div>
+      <div className="flex flex-col grow-1 justify-between">
+        <div className="flex flex-row justify-between items-center bg-gray-200 px-5 py-1">
+          <p className="font-semibold text-xs">
+            {
+              POST_TYPES[
+                type as "additional-content" | "data-panel" | "newsletter"
+              ]
+            }
+          </p>
+          <p className="text-grey-600 text-xs">{formattedDate}</p>
+        </div>
+
+        <div className="py-4 px-5 flex gap-3 justify-between h-full items-center box-border">
+          <p className="line-clamp-2 text-sm font-medium text-wrap">{title}</p>
+          <Icon
+            className="md:flex rotate-270 size-2 min-w-2"
+            id="expand"
+            size={9}
+          />
+        </div>
       </div>
     </Link>
   );
