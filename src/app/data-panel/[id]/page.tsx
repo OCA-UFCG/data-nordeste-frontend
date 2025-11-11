31c1f47851766df407d0d5a8a5742827f3d37455
LeilaFarias <leilafarias1324@gmail.com>
2025-11-11 16:08:24 -0300

    chore: add reviews suggestions

--- src/app/data-panel/[id]/page.tsx
+++ src/app/data-panel/[id]/page.tsx
@@ -3,7 +3,7 @@ import PowerBIContainer from "@/components/PowerBIContainer/PowerBiContainer";
 import HubTemplate from "@/templates/HubTemplate";
 import { REVALIDATE } from "@/utils/constants";
 import { getContent } from "@/utils/contentful";
-import { ReportData } from "@/utils/interfaces";
+import { IPageHeader, ReportData } from "@/utils/interfaces";
 import { DATA_PANEL_QUERY } from "@/utils/queries";
 import { notFound } from "next/navigation";
 
@@ -11,6 +11,7 @@ export const revalidate = REVALIDATE;
 
 interface IDataPanelContent {
   panelsCollection: { items: ReportData[] };
+  pageHeadersCollection: { items: IPageHeader[] };
 }
 
 export default async function DataPanel({
@@ -20,10 +21,10 @@ export default async function DataPanel({
   params: { id: string };
   searchParams: { pageName?: string };
 }) {
-  const { panelsCollection: panels }: IDataPanelContent = await getContent(
-    DATA_PANEL_QUERY,
-    { id: params.id },
-  );
+  const {
+    panelsCollection: panels,
+    pageHeadersCollection: pageHeaders,
+  }: IDataPanelContent = await getContent(DATA_PANEL_QUERY, { id: params.id });
 
   if (!panels.items.length) {
     notFound();
@@ -37,7 +38,10 @@ export default async function DataPanel({
           pageName={searchParams.pageName}
         />
       </div>
-      <AnchorSection macroTheme={panels.items[0].macroTheme} />
+      <AnchorSection
+        macroTheme={panels.items[0].macroTheme}
+        sectionTexts={pageHeaders.items[0]}
+      />
     </HubTemplate>
   );
 }
--- src/components/AnchorSection/AnchorSection.tsx
+++ src/components/AnchorSection/AnchorSection.tsx
@@ -1,8 +1,15 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { themes } from "@/utils/constants";
+import { IPageHeader } from "@/utils/interfaces";
 
-const AnchorSection = ({ macroTheme }: { macroTheme: string }) => {
+const AnchorSection = ({
+  macroTheme,
+  sectionTexts,
+}: {
+  macroTheme: string;
+  sectionTexts?: IPageHeader;
+}) => {
   const isValidTheme = Object.keys(themes).includes(macroTheme);
   const themeSlug = isValidTheme
     ? themes[macroTheme as keyof typeof themes]
@@ -11,14 +18,8 @@ const AnchorSection = ({ macroTheme }: { macroTheme: string }) => {
   return (
     <section className="flex justify-center items-center bg-grey-100 w-full min-h-[260px] border-box">
       <div className="flex flex-col lg:items-start gap-6 px-6 py-9 lg:px-28 lg:py-12 max-w-[1440px]">
-        <h1 className="font-extrabold text-[30px]">
-          Veja os conjuntos de dados relacionados
-        </h1>
-        <h2 className="text-[16px]">
-          Os dados utilizados neste painel estão disponíveis para download no
-          Catálogo de Dados do Data Nordeste. Acesse para explorar as fontes
-          originais, metadados e outras bases relacionadas.
-        </h2>
+        <h1 className="font-extrabold text-[30px]">{sectionTexts?.title}</h1>
+        <h2 className="text-[16px]">{sectionTexts?.subtitle}</h2>
         <Button asChild variant="primary" className="min-w-[300px]">
           <Link href={`/catalog?category=${themeSlug}`}>
             Ir para o catálogo de dados
