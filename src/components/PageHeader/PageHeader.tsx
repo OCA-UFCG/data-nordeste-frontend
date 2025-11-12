0e09f260a5772ec5905cef7dafcbb98c63bc2197
marcelodelazari <marceloalvesdelazari@gmail.com>
2025-11-12 16:01:37 -0300

    Adding 'w-full' Rodrigo's suggestion for better alignment

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -6,7 +6,7 @@ const PageHeader = ({ content }: { content: IPageHeader }) => {
 
   return (
     <section className="flex items-center justify-center bg-grey-100 w-full min-h-[200px] px-6 pt-9 lg:px-20 lg:pt-12 pb-9 border-box">
-      <div className="flex flex-col items-left gap-6 max-w-[1440px]">
+      <div className="flex flex-col items-left gap-6 max-w-[1440px] w-full">
         <h1 className="font-extrabold text-[30px] lg:text-[48px]">{title}</h1>
 
         {richSubtitle ? (
