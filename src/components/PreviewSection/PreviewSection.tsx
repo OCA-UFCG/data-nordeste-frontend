bbd36e56f3370b0752cf17d045649a91af34962f
marcelodelazari <marceloalvesdelazari@gmail.com>
2025-11-19 11:51:46 -0300

    text horizontal alignment fix for mobile

--- src/components/PreviewSection/PreviewSection.tsx
+++ src/components/PreviewSection/PreviewSection.tsx
@@ -64,7 +64,7 @@ const PreviewSection = ({
     <section className="w-full bg-white">
       <div className="w-full max-w-[1440px] mx-auto px-3 lg:px-20">
         <div
-          className="flex flex-col w-full bg-white gap-3 box-border py-5 lg:px-6 py-10 
+          className="flex flex-col w-full bg-white gap-3 box-border py-5 px-3 lg:px-6 py-10 
                  justify-center items-center shadow-md rounded-lg 
                  -translate-y-4 lg:-translate-y-12"
         >
