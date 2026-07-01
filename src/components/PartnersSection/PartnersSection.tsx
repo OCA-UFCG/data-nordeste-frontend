3595920321bcf03d1bea8734888e14ef352d5ef6
Raína Souza <raiina.py@gmail.com>
2026-07-01 17:24:01 -0300

    fix: format text from Contentful

--- src/components/PartnersSection/PartnersSection.tsx
+++ src/components/PartnersSection/PartnersSection.tsx
@@ -7,7 +7,9 @@ const PartnersSection = ({ content }: { content: IPartners }) => {
     <section className="flex justify-center w-full px-6 py-9 lg:px-20">
       <div className="flex flex-col items-left w-full max-w-[1440px] gap-6">
         <h2 className="font-semibold text-3xl">{content.id}</h2>
-        <div>{documentToReactComponents(content.details.json)}</div>
+        <div className="prose max-w-none prose-p:text-base prose-p:font-medium prose-p:leading-6 prose-p:text-[#292829] prose-headings:text-[#292829] prose-h2:text-2xl prose-h2:font-semibold prose-h3:text-xl prose-h3:font-semibold">
+          {documentToReactComponents(content.details.json)}
+        </div>
         <GalleryCarousel
           album={content.albumCollection.items}
           length={"basis-1/1 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"}
